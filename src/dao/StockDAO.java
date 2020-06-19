a47c8ef160bb10b5c2e9109a6f0bb9e3b48e1648
andre.verissimo <lukkalavero@yahoo.com>
2020-06-19 02:18:41 -0300

    Add StockDAO

--- src/dao/StockDAO.java
+++ src/dao/StockDAO.java
@@ -1,5 +1,6 @@
 package dao;
 
+import model.Stock;
 import model.User;
 
 import javax.sql.DataSource;
@@ -8,30 +9,30 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class UserDAO {
+public class StockDAO {
     private DataSource dataSource;
 
-    public UserDAO(DataSource dataSource){
+    public StockDAO(DataSource dataSource){
         this.dataSource = dataSource;
     }
 
-    public ArrayList<User> all(){
+    public ArrayList<Stock> all(){
         try{
-            String query = "SELECT * FROM users";
+            String query = "SELECT * FROM stocks";
             PreparedStatement ps = dataSource.getConnection().prepareStatement(query);
             ResultSet rs = ps.executeQuery();
 
-            ArrayList<User> users = new ArrayList<User>();
+            ArrayList<Stock> stocks = new ArrayList<Stock>();
 
             while(rs.next()) {
-                User user = setUser(rs);
+                Stock stock = setStock(rs);
 
-                users.add(user);
+                stocks.add(stock);
             }
 
             ps.close();
 
-            return users;
+            return stocks;
         }
         catch(SQLException ex){
             System.err.println("Error getting " + ex.getMessage());
@@ -42,18 +43,18 @@ public class UserDAO {
         return null;
     }
 
-    public User find(int id){
+    public Stock find(int id){
         try {
-            String sql = "SELECT * FROM users WHERE id = ? LIMIT 1";
+            String sql = "SELECT * FROM stocks WHERE id = ? LIMIT 1";
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
             ps.setInt(1, id);
             ResultSet rs = ps.executeQuery();
 
-            User user = setUser(rs);
+            Stock stock = setStock(rs);
 
             ps.close();
 
-            return user;
+            return stock;
         }
         catch(SQLException ex){
             System.err.println("Error getting " + ex.getMessage());
@@ -64,15 +65,13 @@ public class UserDAO {
         return null;
     }
 
-    public boolean save(User user){
+    public boolean save(Stock stock){
         try {
-            String sql = "INSERT INTO users (name, cpf, email, birthday)"
-                    + "VALUES(?, ?, ?, ?)";
+            String sql = "INSERT INTO stocks (code, price)"
+                    + "VALUES(?, ?)";
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getCpf());
-            ps.setString(3, user.getEmail());
-            ps.setDate(4, user.getBirthday());
+            ps.setInt(1, stock.getCode());
+            ps.setDouble(2, stock.getPrice());
 
             ps.execute();
             dataSource.getConnection().commit();
@@ -89,17 +88,15 @@ public class UserDAO {
         return false;
     }
 
-    public boolean update(User user){
+    public boolean update(Stock stock){
         try {
-            String sql = "UPDATE users SET name = ?, cpf = ?, email = ?, birthday = ? WHERE id = ?";
+            String sql = "UPDATE stocks SET code = ?, price = ? WHERE id = ?";
 
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
 
-            ps.setString(1, user.getName());
-            ps.setString(2, user.getCpf());
-            ps.setString(3, user.getEmail());
-            ps.setDate(4, user.getBirthday());
-            ps.setInt(5, user.getId());
+            ps.setInt(1, stock.getCode());
+            ps.setDouble(2, stock.getPrice());
+            ps.setInt(3, stock.getId());
 
             ps.execute();
             dataSource.getConnection().commit();
@@ -118,7 +115,7 @@ public class UserDAO {
 
     public boolean delete(int id){
         try {
-            String sql = "DELETE FROM users WHERE id = ?";
+            String sql = "DELETE FROM stocks WHERE id = ?";
             PreparedStatement ps = dataSource.getConnection().prepareStatement(sql);
             ps.setInt(1, id);
             ps.execute();
@@ -136,16 +133,14 @@ public class UserDAO {
         return false;
     }
 
-    private User setUser(ResultSet rs){
+    private Stock setStock(ResultSet rs){
         try {
-            User user = new User();
-            user.setId(rs.getInt("id"));
-            user.setName(rs.getString("name"));
-            user.setCpf(rs.getString("cpf"));
-            user.setEmail(rs.getString("email"));
-            user.setBirthday(rs.getDate("birthday"));
+            Stock stock = new Stock();
+            stock.setId(rs.getInt("id"));
+            stock.setCode(rs.getInt("code"));
+            stock.setPrice(rs.getDouble("price"));
 
-            return user;
+            return stock;
         }
         catch (SQLException ex){
             System.err.println("Error getting " + ex.getMessage());
