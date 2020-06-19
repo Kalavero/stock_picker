91b97b0743b55e0ce3c48881c76824bde643ff7c
andre.verissimo <lukkalavero@yahoo.com>
2020-06-19 00:24:09 -0300

    add stock bean

---
+++ src/models/Stock.java
@@ -0,0 +1,31 @@
+package models;
+
+import java.io.Serializable;
+
+public class Stock implements Serializable {
+    private int id;
+    private double price;
+    private int code;
+
+    public int getId() {
+        return id;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public Stock () {}
+}
