79ad348d93f5aeed1703f9f518a189a9c5759472
andre.verissimo <lukkalavero@yahoo.com>
2020-06-19 00:20:32 -0300

    add wallet bean

---
+++ src/models/Wallet.java
@@ -0,0 +1,40 @@
+package models;
+
+import java.io.Serializable;
+
+public class Wallet implements Serializable {
+    private int id;
+    private int user_id;
+    private String name;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public int getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(int user_id) {
+        this.user_id = user_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Wallet () {}
+}
